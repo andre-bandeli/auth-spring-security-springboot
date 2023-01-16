f28239a3628d6cef5d16c779f4069ab7cdaeb320
andre-bandeli <andre.lbandeli@gmail.com>
2023-01-16 19:52:27 -0300

    test: implementando teste Classe 'UserService'

--- autentication/src/main/java/com/autentication/autentication/AutenticationApplication.java
+++ autentication/src/main/java/com/autentication/autentication/AutenticationApplication.java
@@ -1,12 +1,15 @@
 package com.autentication.autentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AutenticationApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutenticationApplication.class, args);
