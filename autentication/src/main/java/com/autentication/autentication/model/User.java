e1bf0727143966a02285a7b1c965902e66589e3d
andre-bandeli <andre.lbandeli@gmail.com>
2023-01-16 21:08:05 -0300

    test: teste unitário 'authenticateTest' da classe AuthenticationService

--- autentication/src/main/java/com/autentication/autentication/model/User.java
+++ autentication/src/main/java/com/autentication/autentication/model/User.java
@@ -25,6 +25,8 @@ public class User implements UserDetails {
     private Integer id;
     private String firstname;
     private String lastname;
+
+    @Column(unique = true)
     private String email;
 
     @Column(nullable = false)
--- autentication/src/test/java/com/autentication/autentication/AuthenticationServiceTest.java
+++ autentication/src/test/java/com/autentication/autentication/AuthenticationServiceTest.java
@@ -1,8 +1,11 @@
 package com.autentication.autentication;
 
 
+import com.autentication.autentication.config.JwtService;
 import com.autentication.autentication.model.Role;
 import com.autentication.autentication.model.User;
+import com.autentication.autentication.model.auth.AuthenticantionResponse;
+import com.autentication.autentication.model.auth.AuthenticationRequest;
 import com.autentication.autentication.model.auth.RegisterRequest;
 import com.autentication.autentication.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,8 +27,19 @@ public class AuthenticationServiceTest {
     private MockMvc mockMvc;
 
     RegisterRequest request = new RegisterRequest();
+
+    private AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+
+    private AuthenticantionResponse authenticantionResponse = new AuthenticantionResponse();
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    private JwtService jwtService;
     @Autowired
     private UserRepository userRepository;
+
+
 
     @Test
     public void registerTest() {
@@ -47,7 +63,6 @@ public class AuthenticationServiceTest {
     @Test
     public void registerNotNullPasswordTest() {
 
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         String message = null;
 
         var user = User.builder()
@@ -68,11 +83,53 @@ public class AuthenticationServiceTest {
         
         Assertions.assertEquals("Password não pode ser null"  +
                 "\n" + "Password não pode conter espaços em branco", message);
-
     }
 
     @Test
     public void authenticateTest() {
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String message = null;
+
+        var user = User.builder()
+                .firstname("teste")
+                .lastname("teste")
+                .email("teste-14@teste.com")
+                .password(encoder.encode("teste"))
+                .role(Role.ADMIN)
+                .build();
+        userRepository.save(user);
+
+        authenticationRequest.setEmail(user.getEmail());
+        authenticationRequest.setPassword(user.getPassword());
+
+        System.out.println(authenticationRequest.getEmail());
+        System.out.println(authenticationRequest.getPassword());
+
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getEmail(),
+                            authenticationRequest.getPassword()
+                    )
+            );
+
+            var user_autheticate = userRepository.findByEmail(authenticationRequest.getEmail())
+                    .orElseThrow();
+
+            var jwtToken = jwtService.generateToken(user_autheticate);
+
+            AuthenticantionResponse.builder()
+                    .token(jwtToken)
+                    .build();
+
+            message = "User autenticado";
+        }catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        Assertions.assertEquals("User autenticado", message);
     }
 
 }
